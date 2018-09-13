e2e42c5ceed2eb1f181f4aae68871fb9158e872a
Ann <anna.muzykina83@gmail.com>
2018-09-13 19:58:43 +0300

    Add files via upload

---
+++ tower-of-babel/restAndSpread.js
@@ -0,0 +1,12 @@
+//Exercise 12
+
+var args = process.argv[2].split(",");
+args = args.map((arg) => +arg);
+
+// write a function called `avg` here that calculates the average
+var avg = function(...args) {
+	let sum = args.reduce((sum, n) => sum + n);
+	return sum / args.length;
+};
+
+console.log(avg(...args));
